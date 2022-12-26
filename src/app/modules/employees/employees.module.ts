b6dd654b98fcf7b8d93e79574d2c8aaef203c840
DanyaRocketa <hospodi322@gmail.com>
2022-12-26 16:55:49 +0300

    employees servcie and interfaces created

--- src/app/modules/employees/employees.module.ts
+++ src/app/modules/employees/employees.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeesListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    NzTableModule,
   ]
 })
 export class EmployeesModule { }
