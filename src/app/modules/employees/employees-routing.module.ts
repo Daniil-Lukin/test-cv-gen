259c25aeb652dc601b4da90bfd7a69eb9f4f4c74
DanyaRocketa <hospodi322@gmail.com>
2022-12-27 22:03:26 +0300

    Work on component started

--- src/app/modules/employees/employees-routing.module.ts
+++ src/app/modules/employees/employees-routing.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { EmployeesListComponent } from "./components/employees-list/employees-list.component";
 
 const routes: Routes = [
   {
+    path: '',
+    component: EmployeesListComponent,
   },
 ];
 
--- src/app/modules/employees/employees.module.ts
+++ src/app/modules/employees/employees.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { EmpolyeesRoutingModule } from './employees-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    EmpolyeesRoutingModule,
+    NzTableModule,
   ]
 })
 export class EmployeesModule { }
