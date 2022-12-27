160217e174b5a030e63efce271b5e392879b84c8
DanyaRocketa <hospodi322@gmail.com>
2022-12-27 22:10:20 +0300

    Interfaces created

---
+++ src/app/modules/employees/interfaces/position-to-get-interfaces/position-to-get-data.ts
@@ -0,0 +1,6 @@
+import { PositionToGetAttributes } from "./position-to-get-attributes";
+
+export interface PositionToGetData {
+  id: number | string,
+  attributes: PositionToGetAttributes,
+}
---
+++ src/app/modules/employees/interfaces/position-to-get.ts
@@ -0,0 +1,5 @@
+import { PositionToGetData } from "./position-to-get-interfaces/position-to-get-data";
+
+export interface PositionToGet {
+  data: PositionToGetData[] | PositionToGetData,
+}
