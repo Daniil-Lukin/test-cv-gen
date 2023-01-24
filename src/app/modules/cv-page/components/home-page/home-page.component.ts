894dfa5712b95d68fd2305f0208e05cb71dc7158
DanyaRocketa <hospodi322@gmail.com>
2023-01-24 17:05:38 +0300

    logout button added

--- src/app/modules/cv-page/components/home-page/home-page.component.ts
+++ src/app/modules/cv-page/components/home-page/home-page.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { en_US, NzI18nService, ru_RU } from 'ng-zorro-antd/i18n';
+import { enUS, ru } from 'date-fns/locale';
+import { StorageService } from 'src/app/modules/shared/services/storage.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +13,10 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HomePageComponent implements OnInit {
   constructor(
+    private storageService: StorageService,
+    private router: Router,
     private translateService: TranslateService,
+    private i18n: NzI18nService,
   ) {}
 
   ngOnInit(): void {}
@@ -17,8 +24,17 @@ export class HomePageComponent implements OnInit {
   localizationButtonClick() {
     if (this.translateService.store.currentLang === 'en') {
       this.translateService.use('ru');
+      this.i18n.setLocale(ru_RU);
+      this.i18n.setDateLocale(ru);
     } else {
       this.translateService.use('en');
+      this.i18n.setLocale(en_US);
+      this.i18n.setDateLocale(enUS)
     }
+  }
+
+  public logOut() {
+    this.storageService.removeUser();
+    this.router.navigate(['/login']);
   }
 }
